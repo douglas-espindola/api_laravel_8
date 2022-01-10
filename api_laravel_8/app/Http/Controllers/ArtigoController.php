7da937f22b3d691fa269f2462d2b7d2b9d182a30
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-10 00:14:02 -0300

    removido lixo

--- api_laravel_8/app/Http/Controllers/ArtigoController.php
+++ api_laravel_8/app/Http/Controllers/ArtigoController.php
@@ -20,15 +20,6 @@ class ArtigoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -55,16 +46,6 @@ class ArtigoController extends Controller
     {
         $artigo = Artigo::findOrFail($id);
         return new ArtigoResource($artigo);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
     }
 
     /**
